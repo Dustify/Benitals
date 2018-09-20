6fd5a180694bf4715e9b75e30d6f2eab8f1e7a70
Adrian Johnson <aj@dustify.com>
2018-09-20 08:03:08 +0100

    more fuckage

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.listen(port, function () {
-    console.log(`Example app listening on port ${port}!`);
-})
+    //console.log(`Example app listening on port ${port}!`);
+});
